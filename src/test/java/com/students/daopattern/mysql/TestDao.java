709534396cb18f1edd3edd806a9a9448b4ae4449
Vitaliy Simonov <vetsts@gmail.com>
2017-10-22 13:33:08 +0300

    first commit

--- src/test/java/com/students/daopattern/mysql/TestDao.java
+++ src/test/java/com/students/daopattern/mysql/TestDao.java
@@ -1,20 +1,18 @@
-
 package com.students.daopattern.mysql;
 
-
-import com.students.daopattern.dao.PersistException;
 import com.students.daopattern.dao.StudentDao;
 import com.students.daopattern.dto.Mark;
+import com.students.daopattern.dto.TestMark;
 import java.util.List;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class TestDao {
+
     private static StudentDao stud = null;
-    
+
     @BeforeClass
     public static void connectDao() throws Exception {
         stud = new MySqlStudentDao("test");
@@ -26,21 +24,46 @@ public class TestDao {
     }
 
     @Test
-    public void TestDaoConection() {
+    public void testDaoConection() {
         Assert.assertNotNull(stud);
     }
-    
+
     @Test
-    public void TestCreate() throws PersistException {
-        
+    public void testCreate() throws Exception {
         List list = stud.getStudSubjMarkAll();
         int oldListSize = list.size();
-        Assert.assertTrue(oldListSize > 0);
+        Assert.assertTrue("Размер должен быть > 0", oldListSize > 0);
         stud.createMark(new Mark(5, 5, 5));
         list = stud.getStudSubjMarkAll();
         int newListSize = list.size();
-        Assert.assertNotNull(list);
-        Assert.assertEquals(1, newListSize - oldListSize);
+        Assert.assertNotNull("Новый список не должен быть пустым", list);
+        Assert.assertEquals("Размер списка должен быть равен 1", 1, newListSize - oldListSize);
+    }
+
+    @Test
+    public void testUpdateSubject() throws Exception {
+        stud.updateSubject(5, "Информатика");
+        TestMark tm = stud.getTestSubject(5);
+        String subject = tm.getSubject();
+        Assert.assertEquals("Название предмета должно быть Информатика", "Информатика", subject);
+    }
+
+    @Test
+    public void testSelectStudents() throws Exception {
+        List list = stud.getStudents();
+        Assert.assertNotNull("Список студентов не должен быть пустым", list);
+    }
+
+    @Test
+    public void testDeleteMark() throws Exception {
+        List list = stud.getStudSubjMarkAll();
+        int oldListSize = list.size();
+        Assert.assertTrue("Размер должен быть > 0", oldListSize > 0);
+        stud.delete(new Mark(22, 5, 5, 5));
+        list = stud.getStudSubjMarkAll();
+        int newListSize = list.size();
+        Assert.assertNotNull("Новый список не должен быть пустым", list);
+        Assert.assertEquals("Размер списка должен быть равен 1", 1, oldListSize - newListSize);
     }
 
 }
